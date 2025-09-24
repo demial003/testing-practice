5bf8ebe1fa2111a8bdd2f2028687d81547a7efc9
demial003 <demial003@gmail.com>
2025-09-24 15:54:56 -0400

    refactor caeserCipher to more optimal solution

--- modules/caeserCipher.js
+++ modules/caeserCipher.js
@@ -1,33 +1,17 @@
-const caeserCipher = (str, shift) => {
-  let s = str.split("");
-  let res = s.map((el) => {
-    let code = el.charCodeAt(0);
-
-    if (code >= 97 && code <= 122) {
-      if (shift > 0 && code > 122 - shift) {
-        let dist = 122 - code;
-        code = 96 - dist + (shift % 26);
-      } else if (shift < 0 && code < 97 - shift) {
-        let dist = 97 - code;
-        code = 123 - dist + (shift % 26);
-      } else {
-        code += shift % 26;
+const caeserCipher = (str, n) => {
+  return [...str]
+    .map((el) => {
+      let code = el.charCodeAt(0);
+      let shift = n > 0 ? n : 26 + (n % 26);
+      if (code >= 97 && code <= 122) {
+        code = ((code - 97 + shift) % 26) + 97;
       }
-    }
-    if (code >= 65 && code <= 90) {
-      if (shift > 0 && code > 90 - shift) {
-        let dist = 91 - code;
-        code = 64 - dist + (shift % 26);
-      } else if (shift < 0 && code < 65 - shift) {
-        let dist = 65 - code;
-        code = 91 - dist + (shift % 26);
-      } else {
-        code += shift % 26;
+      if (code >= 65 && code <= 90) {
+        code = ((code - 65 + shift) % 26) + 65;
       }
-    }
-    return String.fromCharCode(code);
-  });
-  return res.join("");
+      return String.fromCharCode(code);
+    })
+    .join("");
 };
 
 module.exports = { caeserCipher };
